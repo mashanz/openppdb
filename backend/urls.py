21f2886e7a7f235e572f7a461a95027405dfce8e
mashanz <mangatkk@gmail.com>
2021-03-12 21:38:09 +0700

    Updates Backend default views

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from backend.views import OppdbBackend
 urlpatterns = [
 
     # The home page
-    path('api', OppdbBackend.index, name='backend'),
+    path('', OppdbBackend.index, name='backend'),
 
     # Matches any html file
     re_path(r'^.*\.*', OppdbBackend.pages, name='forbidden'),
--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,42 @@
+import json
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django import template
 
 # Create your views here.
 
 class OppdbBackend:
 
     def index(request):
-        pass
+        data = {
+                "code": 200,
+                "message": "OK",
+                "data": None
+            }
+        dump = json.dumps(data)
+        return HttpResponse(dump, content_type='application/json')
 
     def pages(request):
-        pass
+        context = {}
+        try:
+            load_template      = request.path.split('/')[-1]
+            context['segment'] = load_template
+            html_template = loader.get_template( load_template )
+            return HttpResponse(html_template.render(context, request))
+        except template.TemplateDoesNotExist:
+            data = {
+                "code": 404,
+                "message": "Not Found",
+                "data": None
+            }
+            dump = json.dumps(data)
+            return HttpResponse(dump, content_type='application/json')
+        except:
+            data = {
+                "code": 500,
+                "message": "Server Errors",
+                "data": None
+            }
+            dump = json.dumps(data)
+            return HttpResponse(dump, content_type='application/json')
