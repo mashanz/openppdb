567ecbb0dc8b9ca3898bb29854d77e288b9ecafe
mashanz <mangatkk@gmail.com>
2021-03-12 21:28:05 +0700

    add views

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class OppdbBackend:
+
+    def index(request):
+        pass
+
+    def pages(request):
+        pass
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django import template
 
 # Create your views here.
+
+class OpenPpdbFrontend:
+
+    def index(request):
+        context = {}
+        context['segment'] = 'index'
+        html_template = loader.get_template( 'index.html' )
+        return HttpResponse(html_template.render(context, request))
+
+    def pages(request):
+        context = {}
+        try:
+            load_template      = request.path.split('/')[-1]
+            context['segment'] = load_template
+            html_template = loader.get_template( load_template )
+            return HttpResponse(html_template.render(context, request))
+        except template.TemplateDoesNotExist:
+            html_template = loader.get_template( '404.html' )
+            return HttpResponse(html_template.render(context, request))
+        except:
+            html_template = loader.get_template( '500.html' )
+            return HttpResponse(html_template.render(context, request))
